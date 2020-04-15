2668d7dcd17c126b2a9b12af99dd8d8047f7b969
FDUser <mobilityfd@gmail.com>
2020-04-15 10:54:56 +0530

    Add project files.

---
+++ AspCoreWithCosmosDB/Services/ICosmosDbService.cs
@@ -0,0 +1,15 @@
+﻿using AspCoreWithCosmosDB.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspCoreWithCosmosDB.Services
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
